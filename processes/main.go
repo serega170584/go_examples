c84c77482f05335c9d5f8d879f609d86eabcd3dd
krivobokovsergej <krivobokov_sv@magnit.ru>
2023-06-21 18:01:27 +0300

    processing fix

--- processes/main.go
+++ processes/main.go
@@ -4,65 +4,63 @@ import (
 	"fmt"
 	"math"
 	"math/rand"
-	"time"
 )
-
-// channels allows concurrent processing
-type job struct {
-	value int64
-	state State
-}
-
-type State int
 
 const (
 	InitialState State = iota
-	FirstStage
-	SecondStage
-	FinishedStage
+	FirstState
+	SecondState
+	ThirdState
 )
 
-func FirstProcessing(in <-chan job) chan job {
-	out := make(chan job)
+type State int
 
-	go func() {
-		for j := range in {
-			j.value = int64(float64(j.value) * math.Pi)
-			j.state = FirstStage
-
-			out <- j
-		}
-		close(out)
-	}()
-
-	return out
+type job struct {
+	num   int
+	value int
+	state State
 }
 
-func SecondProcessing(in <-chan job) chan job {
+func FirstProcessing(in chan job) chan job {
 	out := make(chan job)
 
 	go func() {
 		for j := range in {
-			j.value = int64(float64(j.value) * math.E)
-			j.state = SecondStage
-
-			out <- j
-		}
-		close(out)
-	}()
-
-	return out
-}
-
-func LastProcessing(in <-chan job) chan job {
-	out := make(chan job)
-
-	go func() {
-		for j := range in {
-			j.value = int64(float64(j.value) / float64(rand.Intn(10)))
-			j.state = FinishedStage
-
 			fmt.Println(j)
+			j.state = FirstState
+			j.value = int((float64(j.value)*8 + math.Pi) / float64(rand.Intn(9)+1))
+			out <- j
+		}
+		close(out)
+	}()
+
+	return out
+}
+
+func SecondProcessing(in chan job) chan job {
+	out := make(chan job)
+
+	go func() {
+		for j := range in {
+			fmt.Println(j)
+			j.value = int((float64(j.value)*8 + math.E) / float64(rand.Intn(9)+1))
+			j.state = SecondState
+			out <- j
+		}
+		close(out)
+	}()
+
+	return out
+}
+
+func ThirdProcessing(in chan job) chan job {
+	out := make(chan job)
+
+	go func() {
+		for j := range in {
+			fmt.Println(j)
+			j.value = int(j.value * 16 / (rand.Intn(9) + 1))
+			j.state = ThirdState
 			out <- j
 		}
 		close(out)
@@ -72,25 +70,18 @@ func LastProcessing(in <-chan job) chan job {
 }
 
 func main() {
-	length := 50_000_000
-	jobs := make([]job, length)
-	in := make(chan job, len(jobs))
-	for i := 0; i < length; i++ {
-		jobs[i].value = int64(i)
-		in <- jobs[i]
+	cnt := 10
+	in := make(chan job, 10)
+
+	for i := 0; i < cnt; i++ {
+		var j job
+		j.num, j.value = i, i
+		in <- j
 	}
 	close(in)
 
-	start := time.Now()
-
-	result := LastProcessing(
-		SecondProcessing(
-			FirstProcessing(in),
-		),
-	)
-
-	_ = result
-	finished := time.Since(start)
-
-	fmt.Println(finished)
+	out := ThirdProcessing(SecondProcessing(FirstProcessing(in)))
+	for j := range out {
+		fmt.Println(j)
+	}
 }
