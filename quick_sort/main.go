dc22d638db5b63481d594db6fd8efa21d7e6215c
krivobokovsergej <krivobokov_sv@magnit.ru>
2023-06-22 06:45:01 +0300

    processing fix

--- quick_sort/main.go
+++ quick_sort/main.go
@@ -7,61 +7,88 @@ import (
 
 func main() {
 	cnt := 20
-	leftRightValues := make([]leftRightVal, 0)
 	list := make([]int, cnt)
-	for i := 0; i < 20; i++ {
-		list[i] = rand.Intn(30) + 10
-	}
-	list = []int{10, 11, 27, 11, 31, 27, 22, 32, 16, 14, 34, 31, 30, 22, 35, 17, 11, 28, 33, 28}
 
-	leftRightValues = append(leftRightValues, leftRightVal{left: 1, right: cnt - 1})
-	counter := 1
-	currIndex := 0
-	fmt.Println(list)
-loop:
-	for counter > 0 {
-		counter--
-		val := leftRightValues[currIndex]
-		leftInd := val.left
-		rightInd := val.right
-		if leftInd >= rightInd {
-			currIndex++
-			continue loop
-		}
-		num := list[leftInd]
-		fixInd := leftInd
-		leftInd++
-		for leftInd <= rightInd {
-			for leftInd <= val.right && list[leftInd] < num {
-				leftInd++
-			}
-
-			for rightInd >= val.left && list[rightInd] > num {
-				rightInd--
-			}
-
-			if leftInd <= rightInd {
-				list[leftInd], list[rightInd] = list[rightInd], list[leftInd]
-				leftInd++
-				rightInd--
-			}
-		}
-
-		list[fixInd], list[rightInd] = list[rightInd], list[fixInd]
-
-		leftRightValues = append(leftRightValues, leftRightVal{left: fixInd, right: rightInd - 1})
-		counter++
-
-		leftRightValues = append(leftRightValues, leftRightVal{left: rightInd + 1, right: val.right})
-		counter++
-
-		currIndex++
+	for i := 0; i < cnt; i++ {
+		list[i] = rand.Intn(9)
 	}
 
+	list = sort(list)
 	fmt.Println(list)
 }
 
-type leftRightVal struct {
+type lr struct {
 	left  int
 	right int
+}
+
+func sort(list []int) []int {
+	cnt := len(list)
+
+	q := []lr{{left: 0, right: cnt - 1}}
+	curIndex := 0
+	counter := 1
+
+	//list = []int{669, 65, 838, 579, 324, 266, 801, 912, 823, 249, 670, 659, 452, 421, 886, 598, 956, 905, 929, 77}
+
+	fmt.Println(list)
+
+loop:
+	for counter != 0 {
+		counter--
+
+		val := q[curIndex]
+		fmt.Println(val)
+		fmt.Println(list)
+
+		if val.left >= val.right {
+			curIndex++
+			continue loop
+		}
+
+		if val.left == val.right-1 && list[val.left] > list[val.right] {
+			list[val.left], list[val.right] = list[val.right], list[val.left]
+			curIndex++
+			continue loop
+		}
+
+		if val.left == val.right-1 {
+			curIndex++
+			continue loop
+		}
+
+		fix := val.left
+		left := val.left + 1
+		right := val.right
+
+		for left <= right {
+			for left <= val.right && list[fix] >= list[left] {
+				left++
+			}
+
+			for right > fix && list[right] >= list[fix] {
+				right--
+			}
+
+			if left < right {
+				list[left], list[right] = list[right], list[left]
+				left++
+				right--
+			}
+		}
+
+		list[fix], list[right] = list[right], list[fix]
+
+		q = append(q, lr{left: fix, right: right - 1})
+		counter++
+
+		q = append(q, lr{left: right + 1, right: val.right})
+		counter++
+
+		curIndex++
+	}
+
+	fmt.Println(q)
+
+	return list
 }
