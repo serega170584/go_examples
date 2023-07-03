18b0a414f4ea5377f8855420972b02f2ebff6f30
krivobokovsergej <krivobokov_sv@magnit.ru>
2023-07-03 15:19:45 +0300

    server fix

--- server/main.go
+++ server/main.go
@@ -1,6 +1,10 @@
 package main
 
-import "context"
+import (
+	"context"
+	"fmt"
+	"time"
+)
 
 type Server interface {
 	Start()
@@ -8,10 +12,29 @@ type Server interface {
 }
 
 func main() {
-	var s Server
-	ctx := context.Background()
+	var s Serv
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	RunServer(ctx, s)
+	cancel()
+}
+
+type Serv struct{}
+
+func (serv Serv) Start() {
+	fmt.Println("Start")
+}
+
+func (serv Serv) Stop() {
+	fmt.Println("Stop")
+}
+
+func RunServer(ctx context.Context, serv Server) {
+	ch := make(chan struct{})
+	serv.Start()
 	go func(ctx context.Context) {
 		<-ctx.Done()
-		s.Stop()
+		serv.Stop()
+		ch <- struct{}{}
 	}(ctx)
+	<-ch
 }
