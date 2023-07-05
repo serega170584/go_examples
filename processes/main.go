b9ef835d98d6359fb57d0a8709283a3109f6b7e7
krivobokovsergej <krivobokov_sv@magnit.ru>
2023-07-05 09:36:30 +0300

    processes fix

--- processes/main.go
+++ processes/main.go
@@ -8,7 +8,7 @@ import (
 type State int
 
 const (
-	InitialStage State = iota
+	InitialState State = iota
 	FirstStage
 	SecondStage
 	ThirdStage
@@ -17,21 +17,22 @@ const (
 type job struct {
 	num   int
 	state State
-	value int64
+	value int
 }
 
 func main() {
-	cnt := 20
+	cnt := 10
 	in := make(chan job, cnt)
-	for i := 0; i < cnt; i++ {
-		job := job{num: i, value: int64(i)}
-		in <- job
-	}
-	close(in)
+	go func() {
+		for i := 0; i < cnt; i++ {
+			in <- job{num: i, value: i}
+		}
+		close(in)
+	}()
+	res := ThirdProcessing(SecondProcessing(FirstProcessing(in)))
 
-	output := ThirdProcessing(SecondProcessing(FirstProcessing(in)))
-	for job := range output {
-		fmt.Println(job)
+	for val := range res {
+		fmt.Println(val)
 	}
 }
 
@@ -41,7 +42,7 @@ func FirstProcessing(in chan job) chan job {
 		for job := range in {
 			fmt.Println(job)
 			job.state = FirstStage
-			job.value = job.value + 1000
+			job.value = 1000 + int(float64(job.value)*math.Pi)
 			output <- job
 		}
 		close(output)
@@ -55,7 +56,7 @@ func SecondProcessing(in chan job) chan job {
 		for job := range in {
 			fmt.Println(job)
 			job.state = SecondStage
-			job.value = int64(float64(job.value) * math.Pi)
+			job.value *= int(float64(job.value) * math.E)
 			output <- job
 		}
 		close(output)
@@ -69,7 +70,7 @@ func ThirdProcessing(in chan job) chan job {
 		for job := range in {
 			fmt.Println(job)
 			job.state = ThirdStage
-			job.value = int64(float64(job.value) * math.E)
+			job.value = int(job.value / 1000)
 			output <- job
 		}
 		close(output)
