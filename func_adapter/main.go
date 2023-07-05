c9bb9539055d216f8ffad1ca06e9a640113d555c
krivobokovsergej <krivobokov_sv@magnit.ru>
2023-07-05 08:41:46 +0300

    adapter fix

--- func_adapter/main.go
+++ func_adapter/main.go
@@ -9,29 +9,30 @@ import (
 
 func main() {
 	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
-	val, err := adapter(ctx)
+	defer cancel()
+	ch := make(chan struct{})
+	res, err := adapter(ctx, ch)
 	if err != nil {
 		fmt.Println(err)
 	}
-	fmt.Println(val)
-	cancel()
+	fmt.Println(res)
 }
 
 func something() {
 	time.Sleep(time.Duration(rand.Intn(5)) * time.Second)
+	fmt.Println("success")
 }
 
-func adapter(ctx context.Context) (int, error) {
-	ch := make(chan struct{})
+func adapter(ctx context.Context, ch chan struct{}) (int, error) {
 	go func() {
 		something()
 		ch <- struct{}{}
 	}()
 
 	select {
-	case <-ch:
-		return rand.Intn(1000), nil
 	case <-ctx.Done():
 		return 0, ctx.Err()
+	case <-ch:
+		return rand.Intn(1000), nil
 	}
 }
