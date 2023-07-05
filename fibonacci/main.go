d1e93f9a8b23620a7707774c3a0c580c247432a6
krivobokovsergej <krivobokov_sv@magnit.ru>
2023-07-05 11:24:37 +0300

    fibonacci fix

--- fibonacci/main.go
+++ fibonacci/main.go
@@ -1,30 +1,28 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
 
-func fibonacci(c chan int, quit chan int) {
+func main() {
+	cnt := 10
+	ch := make(chan int)
+	quit := make(chan struct{})
+	go func() {
+		for i := 0; i < cnt; i++ {
+			fmt.Println(<-ch)
+		}
+		quit <- struct{}{}
+	}()
+	fibonacci(ch, quit)
+}
+
+func fibonacci(ch chan int, quit chan struct{}) {
 	x, y := 0, 1
 	for {
 		select {
-		case c <- x:
-			x, y = y, x+y
+		case ch <- x:
+			x, y = y, y+x
 		case <-quit:
 			return
 		}
 	}
-}
-
-func main() {
-	c := make(chan int)
-	quit := make(chan int)
-	go func() {
-		for i := 0; i < 10; i++ {
-			fmt.Println(<-c)
-		}
-		quit <- 0
-	}()
-	fibonacci(c, quit)
-	close(c)
 }
