c5ec0d839f0b2475e59a76257b0e11f00969c173
krivobokovsergej <krivobokov_sv@magnit.ru>
2023-08-21 11:37:15 +0300

    cafe refactoring

--- cafe/main.go
+++ cafe/main.go
@@ -3,8 +3,6 @@ package main
 import (
 	"fmt"
 	"log"
-	"strconv"
-	"strings"
 )
 
 // 5 35 40 101 59 63
@@ -15,168 +13,130 @@ import (
 // 8 35 40 101 59 53 101 70 80
 // 8 35 40 101 90 53 101 70 80
 // 1 35
+// 8 35 101 90 101 70 101 80 90
+// 8 35 101 0 0 0 0 0 0
 func main() {
 	var cnt int
 	_, err := fmt.Scanln(&cnt)
+
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	dinners := make([]int, cnt)
-	for i := 0; i < cnt; i++ {
-		_, err = fmt.Scanln(&dinners[i])
+	if cnt == 0 {
+		fmt.Println(0)
+		fmt.Println("0 0")
+		return
+	}
+
+	dayPrices := make([]int, cnt+1)
+
+	for i := 1; i < cnt+1; i++ {
+		_, err = fmt.Scanln(&dayPrices[i])
 		if err != nil {
 			log.Fatal(err)
 		}
 	}
 
-	baseFreeDinnerIndexes := make([]int, 0)
-	secondaryFreeDinnerIndexes := make([]int, 0)
-
-	minBaseFreeDinnerIndex := -1
-	minSecondaryFreeDinnerIndex := -1
-
-	minBaseFreeDinnerIndexOrder := 0
-	minSecondaryFreeDinnerIndexOrder := 0
-
-	var priceSum, freeDinnerSum, secondaryFreeDinnerSum, startInd, notUsedCouponsCnt, secondaryNotUsedCouponsCnt, usedCouponsCnt, secondaryUsedCouponsCnt int
-
-	resultFreeIndexes := make([]string, 0)
-	resultSecondaryFreeIndexes := make([]string, 0)
-
-	for ind, dinnerPrice := range dinners {
-		priceSum += dinnerPrice
-		startInd = ind + 1
-
-		if dinnerPrice > 100 {
-			baseFreeDinnerIndexes = append(baseFreeDinnerIndexes, -1)
-			secondaryFreeDinnerIndexes = append(secondaryFreeDinnerIndexes, -1)
-			break
-		}
-	}
-
-	for ind := startInd; ind < cnt; ind++ {
-		priceSum += dinners[ind]
-		startInd = ind + 1
-
-		if dinners[ind] == 0 {
-			continue
-		}
-
-		minSecondaryFreeDinnerPrice := 0
-		if minSecondaryFreeDinnerIndex != -1 {
-			minSecondaryFreeDinnerPrice = dinners[minSecondaryFreeDinnerIndex]
-		}
-
-		if minSecondaryFreeDinnerPrice < dinners[ind] {
-			minSecondaryFreeDinnerIndex = ind
-			secondaryFreeDinnerIndexes[0] = minSecondaryFreeDinnerIndex
-		}
-
-		if dinners[ind] > 100 {
-			baseFreeDinnerIndexes = append(baseFreeDinnerIndexes, -1)
-			minBaseFreeDinnerIndex = -1
-			minBaseFreeDinnerIndexOrder++
-
-			break
-		}
-
-		minBaseFreeDinnerPrice := 0
-		if minBaseFreeDinnerIndex != -1 {
-			minBaseFreeDinnerPrice = dinners[minBaseFreeDinnerIndex]
-		}
-
-		if minBaseFreeDinnerPrice < dinners[ind] {
-			minBaseFreeDinnerIndex = ind
-			baseFreeDinnerIndexes[0] = minBaseFreeDinnerIndex
-		}
-	}
-
-	for ind := startInd; ind < cnt; ind++ {
-		priceSum += dinners[ind]
-
-		if dinners[ind] == 0 {
-			continue
-		}
-
-		minSecondaryFreeDinnerPrice := 0
-		if minSecondaryFreeDinnerIndex != -1 {
-			minSecondaryFreeDinnerPrice = dinners[minSecondaryFreeDinnerIndex]
-		}
-
-		if minSecondaryFreeDinnerPrice < dinners[ind] {
-			secondaryFreeDinnerIndexes[minSecondaryFreeDinnerIndexOrder] = ind
-			for order, secondaryFreeDinnerIndex := range secondaryFreeDinnerIndexes {
-				if secondaryFreeDinnerIndex == -1 {
-					minSecondaryFreeDinnerIndexOrder = order
-					minSecondaryFreeDinnerIndex = -1
-				} else if minSecondaryFreeDinnerIndex != -1 && dinners[secondaryFreeDinnerIndex] < dinners[minSecondaryFreeDinnerIndex] {
-					minSecondaryFreeDinnerIndexOrder = order
-					minSecondaryFreeDinnerIndex = secondaryFreeDinnerIndex
-				}
-			}
-		}
-
-		if dinners[ind] > 100 {
-			baseFreeDinnerIndexes = append(baseFreeDinnerIndexes, -1)
-			minBaseFreeDinnerIndex = -1
-			minBaseFreeDinnerIndexOrder++
-
-			baseFreeDinnerIndexes, secondaryFreeDinnerIndexes = secondaryFreeDinnerIndexes, baseFreeDinnerIndexes
-			minBaseFreeDinnerIndexOrder, minSecondaryFreeDinnerIndexOrder = minSecondaryFreeDinnerIndexOrder, minBaseFreeDinnerIndexOrder
-			minBaseFreeDinnerIndex, minSecondaryFreeDinnerIndex = minSecondaryFreeDinnerIndex, minBaseFreeDinnerIndex
-
-			continue
-		}
-
-		minBaseFreeDinnerPrice := 0
-		if minBaseFreeDinnerIndex != -1 {
-			minBaseFreeDinnerPrice = dinners[minBaseFreeDinnerIndex]
-		}
-
-		if minBaseFreeDinnerPrice < dinners[ind] {
-			baseFreeDinnerIndexes[minBaseFreeDinnerIndexOrder] = ind
-			for order, baseFreeDinnerIndex := range baseFreeDinnerIndexes {
-				if baseFreeDinnerIndex == -1 {
-					minBaseFreeDinnerIndexOrder = order
-					minBaseFreeDinnerIndex = -1
-				} else if minBaseFreeDinnerIndex != -1 && dinners[baseFreeDinnerIndex] < dinners[minBaseFreeDinnerIndex] {
-					minBaseFreeDinnerIndexOrder = order
-					minBaseFreeDinnerIndex = baseFreeDinnerIndex
-				}
+	prices := make([][]int, cnt+1)
+	for day := 0; day < cnt+1; day++ {
+		prices[day] = make([]int, cnt+2)
+		for couponInd := 0; couponInd < cnt+2; couponInd++ {
+			prices[day][couponInd] = -1
+			if day == 0 && couponInd == 0 {
+				prices[day][couponInd] = 0
 			}
 		}
 	}
 
-	for _, baseFreeDinnerIndex := range baseFreeDinnerIndexes {
-		if baseFreeDinnerIndex == -1 {
-			notUsedCouponsCnt++
-		} else {
-			freeDinnerSum += dinners[baseFreeDinnerIndex]
-			usedCouponsCnt++
-			resultFreeIndexes = append(resultFreeIndexes, strconv.Itoa(baseFreeDinnerIndex+1))
+	minCouponInd := 0
+	minPrice := getPrice(cnt, 0, prices, dayPrices, cnt)
+	for couponInd := 1; couponInd < cnt+2; couponInd++ {
+		price := getPrice(cnt, couponInd, prices, dayPrices, cnt)
+		if price < minPrice {
+			minCouponInd = couponInd
+			minPrice = price
 		}
 	}
 
-	for _, secondaryFreeDinnerIndex := range secondaryFreeDinnerIndexes {
-		if secondaryFreeDinnerIndex == -1 {
-			secondaryNotUsedCouponsCnt++
-		} else {
-			secondaryFreeDinnerSum += dinners[secondaryFreeDinnerIndex]
-			secondaryUsedCouponsCnt++
-			resultSecondaryFreeIndexes = append(resultSecondaryFreeIndexes, strconv.Itoa(secondaryFreeDinnerIndex+1))
+	usedDays := getUsedDays(cnt, minCouponInd, prices, dayPrices, cnt)
+
+	fmt.Println(prices)
+	fmt.Println(minCouponInd)
+	fmt.Println(usedDays)
+}
+
+func getPrice(day int, couponInd int, prices [][]int, dayPrices []int, cnt int) int {
+	price := dayPrices[day]
+
+	if prices[day][couponInd] != -1 {
+		return prices[day][couponInd]
+	}
+
+	if day == 0 && day < couponInd {
+		prices[day][couponInd] = 300
+		return 300
+	}
+
+	if day == 0 {
+		prices[day][couponInd] = 0
+		return 0
+	}
+
+	if couponInd == cnt+1 {
+		prices[day][couponInd] = getPrice(day-1, couponInd, prices, dayPrices, cnt) + 300
+		return prices[day][couponInd]
+	}
+
+	if day < couponInd {
+		prevDayPrice := getPrice(day-1, couponInd, prices, dayPrices, cnt) + 300
+		minPrice := getPrice(day-1, couponInd+1, prices, dayPrices, cnt)
+		if prevDayPrice < minPrice {
+			minPrice = prevDayPrice
 		}
+		prices[day][couponInd] = minPrice
+		return minPrice
 	}
 
-	if freeDinnerSum < secondaryFreeDinnerSum {
-		freeDinnerSum = secondaryFreeDinnerSum
-		notUsedCouponsCnt = secondaryNotUsedCouponsCnt
-		usedCouponsCnt = secondaryUsedCouponsCnt
-		resultFreeIndexes = resultSecondaryFreeIndexes
+	if price > 100 && couponInd == 0 {
+		prices[day][couponInd] = getPrice(day-1, couponInd+1, prices, dayPrices, cnt)
+		return prices[day][couponInd]
 	}
 
-	fmt.Println(priceSum - freeDinnerSum)
-	fmt.Printf("%d %d\n", notUsedCouponsCnt, usedCouponsCnt)
-	fmt.Println(strings.Join(resultFreeIndexes, " "))
+	minPrice := getPrice(day-1, couponInd+1, prices, dayPrices, cnt)
+	prevDayPrice := getPrice(day-1, couponInd, prices, dayPrices, cnt) + price
+	if price > 100 {
+		prevDayPrice = getPrice(day-1, couponInd-1, prices, dayPrices, cnt) + price
+	}
 
+	if prevDayPrice < minPrice {
+		minPrice = prevDayPrice
+	}
+
+	prices[day][couponInd] = minPrice
+	return minPrice
+}
+
+func getUsedDays(day int, couponInd int, prices [][]int, dayPrices []int, cnt int) []int {
+	if day == 0 || couponInd == cnt+1 {
+		usedDays := make([]int, 0)
+		return usedDays
+	}
+
+	price := dayPrices[day]
+	prevCouponInd := couponInd
+	if price > 100 {
+		prevCouponInd = couponInd - 1
+	}
+
+	if prevCouponInd < 0 {
+		return append(getUsedDays(day-1, couponInd+1, prices, dayPrices, cnt), day)
+	}
+
+	diff := prices[day][couponInd] - prices[day-1][prevCouponInd]
+	if diff == price {
+		return getUsedDays(day-1, prevCouponInd, prices, dayPrices, cnt)
+	}
+
+	return append(getUsedDays(day-1, couponInd+1, prices, dayPrices, cnt), day)
 }
