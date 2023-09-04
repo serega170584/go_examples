0904aec4fa3b53d4f4e9735f80ae07d49eb666ea
krivobokovsergej <krivobokov_sv@magnit.ru>
2023-09-04 16:43:22 +0300

    funin add

---
+++ extended_fanin/main.go
@@ -0,0 +1,94 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+type payload struct {
+	name  string
+	value int
+}
+
+func main() {
+	done := make(chan struct{}, 0)
+	wg := sync.WaitGroup{}
+
+	wg.Add(3)
+	producers := make([]<-chan payload, 0, 3)
+	producers = append(producers, producer("Alice", done, &wg))
+	producers = append(producers, producer("Jack", done, &wg))
+	producers = append(producers, producer("Bob", done, &wg))
+
+	fanIn1 := make(chan payload, 0)
+	fanIn2 := make(chan payload, 0)
+
+	wg.Add(3)
+	consumer("C1", producers, done, &wg, fanIn1)
+
+	wg.Add(3)
+	consumer("C2", producers, done, &wg, fanIn2)
+
+	go func() {
+		for {
+			select {
+			case <-done:
+				return
+			case v := <-fanIn1:
+				fmt.Printf("fanIn1 got %v\n", v)
+			case v := <-fanIn2:
+				fmt.Printf("fanIn2 got %v\n", v)
+			}
+		}
+	}()
+
+	time.Sleep(time.Second)
+	close(done)
+	wg.Wait()
+}
+
+func producer(name string, done <-chan struct{}, wg *sync.WaitGroup) <-chan payload {
+	ch := make(chan payload)
+	var i = 1
+	go func() {
+		defer wg.Done()
+		for {
+			select {
+			case <-done:
+				close(ch)
+				fmt.Println(name, "completed")
+				return
+			case ch <- payload{
+				name:  name,
+				value: i,
+			}:
+				fmt.Println(name, "produced", i)
+				i++
+				time.Sleep(time.Millisecond * 500)
+			}
+		}
+	}()
+	return ch
+}
+
+func consumer(name string, channels []<-chan payload, done <-chan struct{}, wg *sync.WaitGroup, fanIn chan<- payload) {
+	for i, ch := range channels {
+		i := i + 1
+		ch := ch
+		go func() {
+			defer wg.Done()
+			fmt.Println("started consumer", name, i)
+			for {
+				select {
+				case <-done:
+					fmt.Println("consumer", name, i, "completed")
+					return
+				case v := <-ch:
+					fmt.Println("consumer", name, i, "got value", v.value, "from", v.name)
+					fanIn <- v
+				}
+			}
+		}()
+	}
+}
---
+++ fanin_multiplexor/main.go
@@ -0,0 +1,107 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+type payload struct {
+	name  string
+	value int
+}
+
+// комбинирует несколько входов в 1 выходной канал
+// иными словами мультиплексируем каналы
+// порядок вывода не гарантируется
+// реальные приложения - хотим лимитировать параллельно выполянемые запросы к внешнему сервису. своего рода rate limiter где в каждый момент времени выполянется 1 запрос
+// простой пример - отправка почты несколько горутин разбирают разные типы очередей формируют тело письма тело сообщения получателей и прочие параметры
+// и 1 горутина получает на обработку все эти сообщения для отправки через внешний сервис в 1 поток
+
+func main() {
+	done := make(chan struct{})
+	wg := sync.WaitGroup{}
+
+	wg.Add(2)
+	producers := make([]<-chan payload, 0, 3)
+	producers = append(producers, producer("Alice", done, &wg))
+	producers = append(producers, producer("Jack", done, &wg))
+
+	fanIn := make(chan payload, 0)
+
+	wg.Add(2)
+	consumer(producers, done, &wg, fanIn)
+
+	go func() {
+		for {
+			select {
+			case <-done:
+				return
+			// читаем чтобы показать результаты
+			case v := <-fanIn:
+				fmt.Printf("fanin got %v\n", v)
+			}
+		}
+	}()
+
+	time.Sleep(time.Second)
+	close(done)
+	wg.Wait()
+}
+
+// возвращает канал, в который мы будем писать полезную нагрузку
+// сигнальный канал done
+// sleep для имитации задержек
+// waitgroup для выполнения graceful shutdown
+func producer(name string, done <-chan struct{}, wg *sync.WaitGroup) <-chan payload {
+	ch := make(chan payload)
+	var i = 1
+	go func() {
+		defer wg.Done()
+		for {
+			select {
+			// оба канала небуферизированные:
+			// выполнение блокируется пока done не будет снаружи закрыт
+			// пока ch не будет снаружи прочитан
+			// при этом в большинстве случаев будет происходить блокировка на запись в канал ch так как горутина consumer читающая канал producer
+			// еще могла не запуститься к этому моменту
+
+			case <-done:
+				close(ch)
+				fmt.Println(name, "completed")
+				return
+			case ch <- payload{
+				name:  name,
+				value: i,
+			}:
+				fmt.Println(name, "produced", i)
+				i++
+				time.Sleep(time.Millisecond * 500)
+			}
+		}
+	}()
+	return ch
+}
+
+func consumer(channels []<-chan payload, done <-chan struct{}, wg *sync.WaitGroup, fanIn chan<- payload) {
+	// для каждого канала создаем горутину для независимого чтения каждого из каналов
+	for i, ch := range channels {
+		i := i + 1
+		ch := ch
+		go func() {
+			defer wg.Done()
+			fmt.Println("started consume of producer", i)
+			for {
+				select {
+				case <-done:
+					fmt.Println("consume of producer", i, "completed")
+					return
+				case v := <-ch:
+					fmt.Println("consumer of producer", i, "got value", v.value, "from", v.name)
+					// все результаты пишем в 1 канал
+					fanIn <- v
+				}
+			}
+		}()
+	}
+}
