3e80e70b4a1b3d1b08214ac428008eae5e73f169
krivobokovsergej <krivobokov_sv@magnit.ru>
2023-09-06 14:04:46 +0300

    romanian add

--- resurrection/main.go
+++ resurrection/main.go
@@ -8,15 +8,29 @@ import (
 	"strings"
 )
 
+//1 4 5 4
+//2 3 6 5
+//3 4 7 8
+//4 5 6 9
+
 func main() {
 	scanner := makeScanner()
 	rowsCnt, colsCnt := readSize(scanner)
 	input := readArr(scanner, rowsCnt, colsCnt)
 
 	output := makeArr(rowsCnt, colsCnt)
+	maxPath := 0
 
-	weights := makeWeights(input, output, 0, 0, rowsCnt, colsCnt)
-	fmt.Println(weights)
+	for i := 0; i < rowsCnt; i++ {
+		for j := 0; j < colsCnt; j++ {
+			output = makeWeights(input, output, i, j, rowsCnt, colsCnt)
+			if output[i][j] > maxPath {
+				maxPath = output[i][j]
+			}
+		}
+	}
+
+	fmt.Println(maxPath)
 }
 
 func makeScanner() *bufio.Scanner {
@@ -71,12 +85,13 @@ func makeWeights(input [][]int, output [][]int, i int, j int, rowsCnt int, colsC
 	weight := -1
 	if i != 0 && input[i][j] > input[i-1][j] {
 		output = makeWeights(input, output, i-1, j, rowsCnt, colsCnt)
-		output[i][j] = output[i-1][j] + input[i][j]
+		weight = output[i-1][j] + 1
+		output[i][j] = weight
 	}
 
 	if i != rowsCnt-1 && input[i][j] > input[i+1][j] {
 		output = makeWeights(input, output, i+1, j, rowsCnt, colsCnt)
-		weight = output[i+1][j] + input[i][j]
+		weight = output[i+1][j] + 1
 	}
 
 	if weight > output[i][j] {
@@ -85,7 +100,7 @@ func makeWeights(input [][]int, output [][]int, i int, j int, rowsCnt int, colsC
 
 	if j != 0 && input[i][j] > input[i][j-1] {
 		output = makeWeights(input, output, i, j-1, rowsCnt, colsCnt)
-		weight = output[i][j-1] + input[i][j]
+		weight = output[i][j-1] + 1
 	}
 
 	if weight > output[i][j] {
@@ -94,7 +109,7 @@ func makeWeights(input [][]int, output [][]int, i int, j int, rowsCnt int, colsC
 
 	if j != colsCnt-1 && input[i][j] > input[i][j+1] {
 		output = makeWeights(input, output, i, j+1, rowsCnt, colsCnt)
-		weight = output[i][j+1] + input[i][j]
+		weight = output[i][j+1] + 1
 	}
 
 	if weight > output[i][j] {
@@ -102,7 +117,7 @@ func makeWeights(input [][]int, output [][]int, i int, j int, rowsCnt int, colsC
 	}
 
 	if weight == -1 {
-		output[i][j] = input[i][j]
+		output[i][j] = 1
 	}
 
 	return output
---
+++ romanian/main.go
@@ -0,0 +1,47 @@
+package main
+
+// xix +
+// xixi - we can't place i after ix
+// xxi + 21
+// xxiv + 24
+// xxix + 29
+// xxx + 30
+// xxxix + 39
+// xl + 40
+// l + 50
+// lx + 60
+// lxx + 70
+// lxxx + 80
+// xc + 90
+// c + 100
+// d + 500
+// m + 1000
+// mmmcd + 3400
+// mmmd + 3500
+// mmmdc + 3600
+// mmmdcc + 3700
+// mmmdccc + 3800
+// mmmcm + 3900
+// mmmm - 4000
+// mmmcmlxxxix  + 3989
+// mm
+// mmccxxii + 2222
+// mmmi + 3001
+// mmmcccxxxiii + 3333
+// mmmcccxxxviii + 3338
+// mmmdccclxxxviii + 3888
+// mmmcccxxxiv + 3334
+// mmcmlxxxix + 2989
+// mcmlxix + 1969
+// cmcd -
+// cml +
+// i v x l c d m
+// i = 1 v = 5 x = 10 l = 50 c = 100 d = 500 m = 1000
+// m d c l x v i
+// m => c, d => c, c => x, l => x, x => i, v => i
+// [m d c l x] - index pointer
+// [2, 4] - index pointer
+// count - we also consider [0 => 0, 1 => 0, 2 => 0, 3 => 0, 4 => 0] with validation
+func main() {
+
+}
