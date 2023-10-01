0bf644d4e1e9776516602cca53bf13e7adc99c37
krivobokovsergej <krivobokov_sv@magnit.ru>
2023-10-01 09:41:54 +0300

    channels merge fix

--- merge_channel/channels_merge.go
+++ merge_channel/channels_merge.go
@@ -2,6 +2,17 @@ package main
 
 import "fmt"
 
+// write to res in cycle, read from res in cycle of main
+// to do read from res in cycle finite we have to close res
+// when close res?
+//   - after working out all input channels => after working out channel 1, channel 2, ...
+//
+// Obviuosly we need count complteted works wuth input channels
+// for that we use channel count of empty struct
+// after doing input channel, write struct to counter
+// in loop from i = 0 to count of input channels read from counter channel
+// after having done cycle we can close res channel so we unlock main goroutine
+// also we close counter after cycle
 func main() {
 	ch1 := make(chan int, 3)
 	ch1 <- 1
