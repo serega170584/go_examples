6ff5640780d95b302a70683718e4fbea85eeea07
krivobokovsergej <krivobokov_sv@magnit.ru>
2023-10-01 15:54:01 +0300

    semaphore wait group add

---
+++ semaphore_queue/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+const DATA_SIZE = 4
+
+func calculate(val int) int {
+	time.Sleep(time.Millisecond * 500)
+	return val * 2
+}
+
+func main() {
+	data := make([]int, 0, DATA_SIZE)
+	for i := 0; i < DATA_SIZE; i++ {
+		data = append(data, i+10)
+	}
+	results := make([]int, DATA_SIZE)
+	semaphore := make(chan struct{}, DATA_SIZE)
+
+	fmt.Printf("Before: %v\n", data)
+	start := time.Now()
+
+	for i, xi := range data {
+		go func(i int, xi int) {
+			results[i] = calculate(xi)
+			semaphore <- struct{}{}
+		}(i, xi)
+	}
+
+	for i := 0; i < DATA_SIZE; i++ {
+		<-semaphore
+	}
+
+	fmt.Printf("After %v\n", results)
+	fmt.Printf("Elapsed: %s\n", time.Since(start))
+}
