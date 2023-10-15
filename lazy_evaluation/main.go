1b748859b9be7faa90e1ace3802d08b49382d2c7
krivobokovsergej <krivobokov_sv@magnit.ru>
2023-10-15 15:03:55 +0300

    lazy evaluation fix

--- lazy_evaluation/main.go
+++ lazy_evaluation/main.go
@@ -6,6 +6,7 @@ import (
 )
 
 type LazyInt func() int
+type EagerInt func() int
 
 func Make(f LazyInt) LazyInt {
 	var v int
@@ -19,6 +20,17 @@ func Make(f LazyInt) LazyInt {
 	}
 }
 
+func MakeEager(f EagerInt) EagerInt {
+	var v int
+	return func() int {
+		func() {
+			v = f()
+			//f = nil
+		}()
+		return v
+	}
+}
+
 func main() {
 	n := Make(func() int {
 		fmt.Println("Doing expensive calculations")
@@ -26,4 +38,12 @@ func main() {
 	})
 	fmt.Println(n())
 	fmt.Println(n() + 42)
+
+	p := MakeEager(func() int {
+		fmt.Println("Doing expensive calculations")
+		return 23
+	})
+
+	fmt.Println(p())
+	fmt.Println(p() + 42)
 }
