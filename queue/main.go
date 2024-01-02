03aa8ca6df41970b313504e5bd4ec132828fe8e3
krivobokov <krivobokov_sv@magnit.ru>
2024-01-02 19:59:06 +0300

    queue fix

--- queue/main.go
+++ queue/main.go
@@ -2,41 +2,35 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
 	"sync"
 	"time"
 )
 
-const N = 3
-const MESSAGES = 10
+const queueCnt = 3
+const messagesCnt = 10
 
 func main() {
-	var wg sync.WaitGroup
-
-	fmt.Println("Queue of length N:", N)
-	queue := make(chan struct{}, N)
-
-	wg.Add(MESSAGES)
-
-	for w := 1; w <= MESSAGES; w++ {
-		process(w, queue, &wg)
+	queue := make(chan struct{}, queueCnt)
+	wg := &sync.WaitGroup{}
+	for i := 0; i < messagesCnt; i++ {
+		num := rand.Intn(1000)
+		fmt.Println("Created num ", num)
+		process(num, queue, wg)
 	}
-
 	wg.Wait()
-
 	close(queue)
-	fmt.Println("Processing completed")
 }
 
-func process(payload int, queue chan struct{}, wg *sync.WaitGroup) {
+func process(val int, queue chan struct{}, wg *sync.WaitGroup) {
 	queue <- struct{}{}
 
-	go func() {
+	wg.Add(1)
+	go func(wg *sync.WaitGroup, val int) {
 		defer wg.Done()
-
-		fmt.Printf("Start processing of %d\n", payload)
-		time.Sleep(time.Millisecond * 500)
-		fmt.Printf("Completed processing of %d\n", payload)
-
+		fmt.Println("Processing start ", val)
+		time.Sleep(500 * time.Millisecond)
+		fmt.Println("Processing finish", val)
 		<-queue
-	}()
+	}(wg, val)
 }
