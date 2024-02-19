55ce045f43d64277018d562539ee61d240804484
krivobokov <krivobokov_sv@magnit.ru>
2024-02-19 08:39:53 +0300

    maximize steps path algorithm add

--- maximize_steps/main.go
+++ maximize_steps/main.go
@@ -27,22 +27,50 @@ func main() {
 	scanner.Scan()
 	stepsCnt, _ := strconv.Atoi(scanner.Text())
 
-	fmt.Println("Got max cost", maxCost(n, list, stepsCnt))
+	cost, path := maxCost(n, list, stepsCnt)
+	fmt.Println("Got max cost", cost)
+
+	fmt.Println("Got path", path)
 }
 
-func maxCost(n int, list []int, stepsCnt int) int {
+func maxCost(n int, list []int, stepsCnt int) (int, []int) {
 	cost := make([]int, n)
 	cost[0] = list[0]
+	path := make([]int, n)
 	for i := 1; i < n; i++ {
 		maxVal := -math.MaxInt
 		for j := 0; j < stepsCnt; j++ {
 			prev := i - j - 1
 			if prev >= 0 {
-				maxVal = max(cost[prev]+list[i], maxVal)
+				if cost[prev]+list[i] > maxVal {
+					maxVal = cost[prev] + list[i]
+					path[i] = prev
+				}
 			}
 		}
 		cost[i] = maxVal
 	}
+
 	fmt.Println(cost)
-	return cost[n-1]
+	fmt.Println(path)
+
+	prevPath := make([]int, n)
+	ind := n - 1
+	cnt := 0
+	for ind != 0 {
+		prevPath[ind] = path[ind]
+		ind = path[ind]
+		cnt++
+	}
+
+	resultPath := make([]int, cnt-1)
+	ind = 0
+	for _, v := range prevPath {
+		if v != 0 {
+			resultPath[ind] = v
+			ind++
+		}
+	}
+
+	return cost[n-1], resultPath
 }
