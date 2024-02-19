22e36ce1ee0728e68c07ac2f43f44fb43d16d7e6
krivobokov <krivobokov_sv@magnit.ru>
2024-02-19 09:21:15 +0300

    max subsequence algorithm add

---
+++ max_growing_subsequence/main.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+func main() {
+	scanner := bufio.NewScanner(os.Stdin)
+	scanner.Split(bufio.ScanWords)
+
+	fmt.Println("Enter count")
+	scanner.Scan()
+	n, _ := strconv.Atoi(scanner.Text())
+
+	fmt.Println("Enter list")
+	list := make([]int, n)
+	for i := 0; i < n; i++ {
+		scanner.Scan()
+		list[i], _ = strconv.Atoi(scanner.Text())
+	}
+
+	fmt.Println("Got max subsequence length", maxGrowingSubsequence(n, list))
+}
+
+func maxGrowingSubsequence(n int, list []int) int {
+	maxVal := 1
+	maxList := make([]int, n)
+
+	for i := range maxList {
+		maxList[i] = 1
+	}
+
+	for i := 1; i < n; i++ {
+		for j := 0; j < i; j++ {
+			if list[i] > list[j] {
+				val := maxList[j] + 1
+				if val > maxList[i] {
+					maxList[i] = val
+				}
+			}
+		}
+		maxVal = max(maxList[i], maxVal)
+	}
+
+	fmt.Println(maxList)
+
+	return maxVal
+}
