e7ca6c3e343f198a38b796b28426504c6c0c60ff
krivobokov <krivobokov_sv@magnit.ru>
2024-02-21 20:24:04 +0300

    max turtle path without debts algorithm add

--- turtle_max_path/main.go
+++ turtle_max_path/main.go
@@ -30,7 +30,7 @@ func main() {
 		}
 	}
 
-	fmt.Println("Got turtle min cost", turtleMaxCost(m, n, list))
+	fmt.Println("Got turtle min x without debt", minWithoutDebtX(m, n, list))
 }
 
 func turtleMaxCost(m int, n int, list [][]int) int {
@@ -48,13 +48,50 @@ func turtleMaxCost(m int, n int, list [][]int) int {
 		for j := range list[i] {
 			prevI := i - 1
 			prevJ := j - 1
-			if prevI >= 0 {
+			if prevI >= 0 && maxList[prevI][j] >= 0 {
 				maxList[i][j] = max(maxList[prevI][j]+list[i][j], maxList[i][j])
 			}
-			if prevJ >= 0 {
+			if prevJ >= 0 && maxList[i][prevJ] >= 0 {
 				maxList[i][j] = max(maxList[i][prevJ]+list[i][j], maxList[i][j])
 			}
 		}
 	}
 	return maxList[m-1][n-1]
+}
+
+func minWithoutDebtX(m int, n int, list [][]int) int {
+	minPath := 0
+	maxPath := 0
+	for _, row := range list {
+		for _, v := range row {
+			if v > 0 {
+				maxPath += v
+			}
+			if v < 0 {
+				minPath += v
+			}
+		}
+	}
+	l := -maxPath
+	r := -minPath
+
+	for l < r {
+		mid := (l + r) / 2
+		if check(mid, m, n, list) {
+			l = mid + 1
+		} else {
+			r = mid
+		}
+	}
+
+	return l
+}
+
+func check(mid int, m int, n int, list [][]int) bool {
+	list[0][0] = mid
+	cost := turtleMaxCost(m, n, list)
+	if cost < 0 {
+		return true
+	}
+	return false
 }
