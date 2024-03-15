d453913c316aed5a1dc3f0eba79b049bff4145b1
krivobokov <krivobokov_sv@magnit.ru>
2024-03-15 19:32:08 +0300

    min race algorithm add

---
+++ min_race_class/main.go
@@ -0,0 +1,71 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"math"
+	"os"
+	"strconv"
+)
+
+func main() {
+	scanner := bufio.NewScanner(os.Stdin)
+	scanner.Split(bufio.ScanWords)
+
+	scanner.Scan()
+	n, _ := strconv.Atoi(scanner.Text())
+
+	scanner.Scan()
+	m, _ := strconv.Atoi(scanner.Text())
+
+	a := make([][]int, n)
+	for i := 0; i < n; i++ {
+		a[i] = make([]int, m)
+		for j := 0; j < m; j++ {
+			scanner.Scan()
+			a[i][j], _ = strconv.Atoi(scanner.Text())
+		}
+	}
+
+	fmt.Println(maxDeny(n, m, a))
+}
+
+func maxDeny(n int, m int, a [][]int) (int, int) {
+	maxVal := math.MinInt
+	for i := 0; i < n; i++ {
+		for j := 0; j < m; j++ {
+			maxVal = max(a[i][j], maxVal)
+		}
+	}
+
+	minExcludeMaxVal := maxVal
+	row := -1
+	col := -1
+	for i := 0; i < n; i++ {
+		for j := 0; j < m; j++ {
+			val := getExcludeMaxVal(i, j, n, m, a, minExcludeMaxVal)
+			if val < minExcludeMaxVal {
+				minExcludeMaxVal = val
+				row = i + 1
+				col = j + 1
+			}
+		}
+	}
+
+	return row, col
+}
+
+func getExcludeMaxVal(row int, col int, n int, m int, a [][]int, minExcludeMaxVal int) int {
+	maxVal := 0
+	for i := 0; i < n; i++ {
+		for j := 0; j < m; j++ {
+			if i != row && j != col {
+				maxVal = max(maxVal, a[i][j])
+				if maxVal >= minExcludeMaxVal {
+					return minExcludeMaxVal
+				}
+			}
+		}
+	}
+	return maxVal
+}
