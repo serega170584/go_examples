b56db5e8200713f25a12c1e1d783131ffbfe8ab0
krivobokov <krivobokov_sv@magnit.ru>
2024-03-27 22:47:31 +0300

    binary search algorithm add

--- number_interval/main.go
+++ number_interval/main.go
@@ -1,17 +1,71 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"slices"
+	"strconv"
+	"strings"
+)
 
 func main() {
-	list := []int{1, 2, 3, 4, 5, 6, 10, 10, 10, 10}
-	fmt.Println(rbinarySearch(10, 10, list))
+	scanner := bufio.NewScanner(os.Stdin)
+	scanner.Split(bufio.ScanWords)
+
+	scanner.Scan()
+	n, _ := strconv.Atoi(scanner.Text())
+
+	intervals := make([]int, 0)
+	for i := 0; i < n; i++ {
+		scanner.Scan()
+		val, _ := strconv.Atoi(scanner.Text())
+		intervals = append(intervals, val)
+	}
+
+	scanner.Scan()
+	k, _ := strconv.Atoi(scanner.Text())
+
+	list := make([][2]int, k)
+	for i := 0; i < k; i++ {
+		scanner.Scan()
+		list[i][0], _ = strconv.Atoi(scanner.Text())
+		scanner.Scan()
+		list[i][1], _ = strconv.Atoi(scanner.Text())
+	}
+
+	lengths := getIntervalLengths(n, list, intervals)
+	s := make([]string, n)
+	for i, v := range lengths {
+		s[i] = strconv.Itoa(v)
+	}
+
+	fmt.Println(strings.Join(s, " "))
+}
+
+func getIntervalLengths(n int, list [][2]int, intervals []int) []int {
+	slices.Sort(intervals)
+
+	lengths := make([]int, 0)
+	for _, v := range list {
+		l := binarySearch(v[0], n, intervals)
+		r := rbinarySearch(v[1], n, intervals)
+		length := 0
+		if r >= l {
+			length = r - l + 1
+		}
+		lengths = append(lengths, length)
+	}
+
+	return lengths
 }
 
 func binarySearch(x int, n int, list []int) int {
 	l := 0
 	r := n - 1
+	m := 0
 	for l < r {
-		m := (l + r) / 2
+		m = (l + r) / 2
 		if check(m, x, list) {
 			r = m
 		} else {
@@ -27,9 +81,9 @@ func rbinarySearch(x int, n int, list []int) int {
 	for l < r {
 		m := (l + r + 1) / 2
 		if lcheck(m, x, list) {
-			l = m + 1
+			l = m
 		} else {
-			l = r
+			r = m - 1
 		}
 	}
 	return l
