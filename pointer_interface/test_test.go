a0d59640be59765fef1b02733931ee39b4467ef7
krivobokov <krivobokov_sv@magnit.ru>
2024-04-05 16:02:47 +0300

    pointer interface test add

---
+++ pointer_interface/test_test.go
@@ -0,0 +1,119 @@
+package main
+
+import (
+	"fmt"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+type TestInterface interface {
+	A()
+}
+
+type Test struct{}
+
+func (t *Test) A() {
+	fmt.Println("123")
+}
+
+func TestGeneral(t *testing.T) {
+	tests := []struct {
+		name     string
+		in       any
+		expected any
+	}{
+		{
+			name:     "first test",
+			in:       1,
+			expected: 1,
+		},
+		{
+			name:     "first test",
+			in:       1.00,
+			expected: 1.00,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			General(1)
+			assert.Equal(t, tt.expected, General(tt.in))
+		})
+	}
+}
+
+func TestFalseInterfaceCompare(t *testing.T) {
+	tests := []struct {
+		name string
+	}{
+		{
+			name: "first test",
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			General(1)
+			assert.Equal(t, false, modifyInterface())
+		})
+	}
+}
+
+func TestInterfacePointerCompare(t *testing.T) {
+	tests := []struct {
+		name string
+	}{
+		{
+			name: "first test",
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			General(1)
+			assert.Equal(t, false, modifyInterface())
+		})
+	}
+}
+
+func TestModifyInterfacePointer(t *testing.T) {
+	tests := []struct {
+		name string
+	}{
+		{
+			name: "first test",
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			General(1)
+			assert.Equal(t, false, modifyInterfacePointer())
+		})
+	}
+}
+
+func General(a any) any {
+	return a
+}
+
+func modifyInterface() bool {
+	var a interface{}
+	a = 1
+	var b interface{}
+	b = 1.00
+	return a == b
+}
+
+func modifyInterfacePointer() bool {
+	var a *Test
+	//var a TestInterface
+	//var a *int
+	//var b interface{}
+	//b = 1.00
+	return nilA(a)
+}
+
+func nilA(a TestInterface) bool {
+	return a.(*Test) == nil
+}
