4bd0e5ac25ad26dafc2416be2ce99f76e3867f3a
krivobokov <krivobokov_sv@magnit.ru>
2024-04-05 17:34:34 +0300

    foo bar fix

--- foobar/main.go
+++ foobar/main.go
@@ -2,41 +2,39 @@ package main
 
 import (
 	"fmt"
-	"sync/atomic"
-	"time"
+	"sync"
 )
 
-func printFoo(handledFooCnt *int64, handledBarCnt *int64) {
-	for i := 0; i < 10; i++ {
-		for *handledFooCnt != *handledBarCnt {
-			continue
-		}
-		fmt.Println("Foo")
-		atomic.AddInt64(handledFooCnt, 1)
-	}
-}
-
-func printBar(handledFooCnt *int64, handledBarCnt *int64, ch <-chan struct{}) {
-	for i := 0; i < 10; i++ {
-		for *handledFooCnt != *handledBarCnt+1 {
-			continue
-		}
-		fmt.Println("Bar")
-		atomic.AddInt64(handledBarCnt, 1)
-	}
-	<-ch
-}
-
 func main() {
-	t := time.Now()
-	var handledFooCnt, handledBarCnt *int64
-	var a, b int64
-	handledFooCnt = &a
-	handledBarCnt = &b
-	ch := make(chan struct{})
-	go printFoo(handledFooCnt, handledBarCnt)
-	go printBar(handledFooCnt, handledBarCnt, ch)
-	ch <- struct{}{}
-	close(ch)
-	fmt.Println(time.Since(t))
+	semBar := make([]chan struct{}, 10)
+	semFoo := make([]chan struct{}, 11)
+	for i := 0; i < 10; i++ {
+		semBar[i] = make(chan struct{})
+		semFoo[i] = make(chan struct{})
+	}
+	semFoo[10] = make(chan struct{})
+	go func() {
+		semFoo[0] <- struct{}{}
+		<-semFoo[10]
+	}()
+	wg := &sync.WaitGroup{}
+	wg.Add(2)
+	go func() {
+		defer wg.Done()
+		for i := 0; i < 10; i++ {
+			<-semFoo[i]
+			fmt.Println("Foo")
+			semBar[i] <- struct{}{}
+		}
+	}()
+
+	go func() {
+		defer wg.Done()
+		for i := 0; i < 10; i++ {
+			<-semBar[i]
+			fmt.Println("Bar")
+			semFoo[i+1] <- struct{}{}
+		}
+	}()
+	wg.Wait()
 }
