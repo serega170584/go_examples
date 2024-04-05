03e8dcbab1736873a3cb9215dc111a8e6f144529
krivobokov <krivobokov_sv@magnit.ru>
2024-04-05 16:28:44 +0300

    pointer interface test add

--- pointer_interface/test_test.go
+++ pointer_interface/test_test.go
@@ -88,7 +88,7 @@ func TestModifyInterfacePointer(t *testing.T) {
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			General(1)
-			assert.Equal(t, false, modifyInterfacePointer())
+			assert.Equal(t, true, modifyInterfacePointer())
 		})
 	}
 }
