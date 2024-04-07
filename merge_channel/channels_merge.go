d2eaa980f14690705d3ae41cdb13c6253cb76d0f
krivobokov <krivobokov_sv@magnit.ru>
2024-04-07 07:30:44 +0300

    channels merge fix

--- merge_channel/channels_merge.go
+++ merge_channel/channels_merge.go
@@ -8,47 +8,48 @@ func main() {
 	a <- 2
 	a <- 3
 	close(a)
-	b := make(chan int, 2)
+	b := make(chan int, 4)
+	b <- 1
 	b <- 2
 	b <- 3
+	b <- 4
 	close(b)
-	c := make(chan int, 4)
+	c := make(chan int, 5)
+	c <- 1
+	c <- 2
+	c <- 3
 	c <- 4
-	c <- 5
-	c <- 6
-	c <- 7
 	close(c)
 
-	o := mergeChannels(a, b, c)
+	out := mergeChannels(a, b, c)
 
-	for v := range o {
+	for v := range out {
 		fmt.Println(v)
 	}
 }
 
 func mergeChannels(chList ...chan int) chan int {
-	output := make(chan int)
+	out := make(chan int)
 	sem := make([]chan struct{}, len(chList))
-
-	for i := range chList {
+	for i := range sem {
 		sem[i] = make(chan struct{})
 	}
 
 	for i, ch := range chList {
-		go func(ch chan int, output chan int, i int, sem []chan struct{}) {
+		go func(ch chan int, out chan int, sem []chan struct{}, i int) {
 			for v := range ch {
-				output <- v
+				out <- v
 			}
 			sem[i] <- struct{}{}
-		}(ch, output, i, sem)
+		}(ch, out, sem, i)
 	}
 
-	go func(sem []chan struct{}, output chan int) {
+	go func(sem []chan struct{}, out chan int) {
 		for _, v := range sem {
 			<-v
 		}
-		close(output)
-	}(sem, output)
+		close(out)
+	}(sem, out)
 
-	return output
+	return out
 }
