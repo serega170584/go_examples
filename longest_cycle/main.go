fc955e7b7f89672dd8d3aaa6b1df8f315c5b3c52
krivobokov <krivobokov_sv@magnit.ru>
2024-05-12 13:59:31 +0300

    longest cycle fix

--- longest_cycle/main.go
+++ longest_cycle/main.go
@@ -1,73 +1,52 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
-	"os"
 	"strconv"
-	"time"
+	"strings"
 )
 
 // RDRU
 // [] R [] D [] R [] U []
 // [0 0] [1 0] [1 1] [2 1] [2 0]
 func main() {
-	start := time.Now()
+	a := []string{"U", "L", "D", "R", "U", "L", "D", "R"}
+	fmt.Println(getLongestCycle(a))
+}
 
-	scanner := bufio.NewScanner(os.Stdin)
-	scanner.Split(bufio.ScanWords)
-	scanner.Scan()
-	cnt, _ := strconv.Atoi(scanner.Text())
+func getLongestCycle(a []string) int {
+	al := len(a)
+	m := make(map[string]int, al)
 
-	list := make([]string, cnt)
-	for i := 0; i < cnt; i++ {
-		scanner.Scan()
-		list[i] = scanner.Text()
-	}
-
-	locationNum := make(map[string]int, cnt+1)
-
-	x, y := 0, 0
-	ind := fmt.Sprintf("%d%d", x, y)
-	num := 0
-	locationNum[ind] = num
-	max := 0
-
-	for i := 0; i < cnt; i++ {
-		num++
-
-		way := list[i]
-
-		if way == "L" {
-			x -= 1
+	left := 0
+	top := 0
+	m[strings.Join([]string{strconv.Itoa(left), strconv.Itoa(top)}, "_")] = 0
+	mc := 0
+	for i, v := range a {
+		if v == "L" {
+			left--
 		}
 
-		if way == "D" {
-			y += 1
+		if v == "R" {
+			left++
 		}
 
-		if way == "R" {
-			x += 1
+		if v == "U" {
+			top--
 		}
 
-		if way == "U" {
-			y -= 1
+		if v == "D" {
+			top++
 		}
 
-		ind = fmt.Sprintf("%d%d", x, y)
+		k := strings.Join([]string{strconv.Itoa(left), strconv.Itoa(top)}, "_")
 
-		if _, ok := locationNum[ind]; !ok {
-			locationNum[ind] = num
-		}
-
-		cycleLen := num - locationNum[ind]
-
-		if cycleLen > max {
-			max = cycleLen
+		if _, ok := m[k]; ok {
+			mc = max(mc, i-m[k]+1)
+		} else {
+			m[k] = i + 1
 		}
 	}
 
-	fmt.Println("Max cycle length: ", max)
-
-	fmt.Println("Estimated time: ", time.Since(start))
+	return mc
 }
