54de1d8e247b6cd5ce660f77e98622ab26ed4067
krivobokov <krivobokov_sv@magnit.ru>
2024-06-01 17:39:11 +0300

    image search fix

--- image_search/main.go
+++ image_search/main.go
@@ -24,43 +24,34 @@ func main() {
 }
 
 func searchImage(iw int, ih int, image [][]int, sw int, sh int, search [][]int) (int, int) {
-	searched := make(map[string]bool, len(image))
+	m := make(map[string]bool)
 	topRes := -1
 	leftRes := -1
-	for i, row := range image {
-		for j, v := range row {
-			for sk, s := range searched {
-				if s == true {
-					parts := strings.Split(sk, "_")
-					top, _ := strconv.Atoi(parts[0])
-					left, _ := strconv.Atoi(parts[1])
-					tsp := i - top
-					lsp := j - left
-					if tsp == sh-1 && lsp == sw-1 && search[tsp][lsp] == v {
+	for i, r := range image {
+		for j, v := range r {
+			for k, mv := range m {
+				if mv {
+					parts := strings.Split(k, "_")
+					t, _ := strconv.Atoi(parts[0])
+					l, _ := strconv.Atoi(parts[1])
+					st := i - t
+					sl := j - l
+					if 0 <= st && st < sh && 0 <= sl && sl < sw && search[st][sl] != v {
+						m[k] = false
+						continue
+					}
+					if st == sh-1 && sl == sw-1 {
+						m[k] = false
 						topRes = i
 						leftRes = j
-						searched[sk] = false
-						continue
-					}
-					if 0 <= tsp && tsp < sh && 0 <= lsp && lsp < sw && search[tsp][lsp] != v {
-						searched[sk] = false
-						continue
-					}
-					if tsp > sh-1 {
-						continue
-					}
-					if lsp > sw-1 {
-						continue
 					}
 				}
 			}
-			if v == search[0][0] {
-				if i+sh-1 < ih && j+sw-1 < iw {
-					searched[strings.Join([]string{strconv.Itoa(i), strconv.Itoa(j)}, "_")] = true
-				}
+			if search[0][0] == v && i+sh-1 < ih && j+sw-1 < iw {
+				k := strings.Join([]string{strconv.Itoa(i), strconv.Itoa(j)}, "_")
+				m[k] = true
 			}
 		}
 	}
-
 	return topRes, leftRes
 }
