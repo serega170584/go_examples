f715885edd323fcabf52fc51397666b40b273377
krivobokov <krivobokov_sv@magnit.ru>
2024-06-02 17:05:36 +0300

    sync cond add

---
+++ sync_cond/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+func main() {
+	c := sync.NewCond(&sync.Mutex{})
+	data := ""
+	go func() {
+		c.L.Lock()
+		c.Wait()
+		fmt.Println(data)
+		c.L.Unlock()
+	}()
+	go func() {
+		c.L.Lock()
+		c.Wait()
+		fmt.Println(data)
+		c.L.Unlock()
+	}()
+	go func() {
+		c.L.Lock()
+		data = "123123123"
+		c.Broadcast()
+		c.L.Unlock()
+	}()
+	time.Sleep(3 * time.Second)
+
+}
