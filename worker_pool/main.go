429a53394295c674cbd2bfe0d103d62dea1070b4
krivobokov <krivobokov_sv@magnit.ru>
2024-06-16 14:31:41 +0300

    worker pool fix

--- worker_pool/main.go
+++ worker_pool/main.go
@@ -8,28 +8,29 @@ import (
 
 func main() {
 	in := make(chan int, 1000)
+	out := make(chan string)
+
 	for i := 0; i < 1000; i++ {
 		in <- i
 	}
 	close(in)
 
-	wg := sync.WaitGroup{}
-	wg.Add(5)
+	wg := &sync.WaitGroup{}
+	wg.Add(4)
 
-	out := make(chan string)
+	for i := 0; i < 4; i++ {
+		go func(i int) {
+			defer wg.Done()
+			for v := range in {
+				out <- "Task: " + strconv.Itoa(i) + ", value: " + strconv.Itoa(v)
+			}
+		}(i)
+	}
+
 	go func() {
 		wg.Wait()
 		close(out)
 	}()
-
-	for i := 0; i < 5; i++ {
-		go func(i int) {
-			defer wg.Done()
-			for v := range in {
-				out <- "Result: " + strconv.Itoa(v) + ", job: " + strconv.Itoa(i)
-			}
-		}(i)
-	}
 
 	for v := range out {
 		fmt.Println(v)
