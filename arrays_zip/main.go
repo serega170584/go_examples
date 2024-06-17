774d777aeeb27cae3262baa93f2cda3084c7c200
krivobokov <krivobokov_sv@magnit.ru>
2024-06-17 14:22:38 +0300

    arrays zip fix

--- arrays_zip/main.go
+++ arrays_zip/main.go
@@ -1,30 +1,24 @@
 package main
 
-import (
-	"fmt"
-	"math"
-)
+import "fmt"
 
 func main() {
-	fmt.Println(arrayZip([]int{1, 2, 2}, []int{2, 3}))
+	fmt.Println(zip([]int{1, 2}, []int{3, 4, 5}, []int{5, 6, 7, 8, 9}))
 }
 
-func arrayZip(list ...[]int) [][]int {
-	l := math.MaxInt
-	for _, a := range list {
-		l = min(l, len(a))
+func zip(a ...[]int) [][]int {
+	l := len(a)
+	ml := len(a[0])
+	for i := 1; i < l; i++ {
+		ml = min(ml, len(a[i]))
 	}
 
-	res := make([][]int, l)
+	res := make([][]int, ml)
 
-	ll := len(list)
-	for i := 0; i < l; i++ {
-		res[i] = make([]int, ll)
-	}
-
-	for i := 0; i < l; i++ {
-		for j := 0; j < ll; j++ {
-			res[i][j] = list[j][i]
+	for i := 0; i < ml; i++ {
+		res[i] = make([]int, 0)
+		for j := 0; j < l; j++ {
+			res[i] = append(res[i], a[j][i])
 		}
 	}
 
