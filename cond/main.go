3816dbd5924782da0ae4bc92f8846b33989ead53
krivobokov <krivobokov_sv@magnit.ru>
2024-06-18 18:57:59 +0300

    sync cond fix

--- cond/main.go
+++ cond/main.go
@@ -6,25 +6,46 @@ import (
 	"time"
 )
 
-func main() {
-	ch := make(chan struct{})
-	mu := &sync.Mutex{}
-	c := sync.NewCond(mu)
-	go func(c *sync.Cond, ch chan struct{}) {
-		c.L.Lock()
-		c.Wait()
-		fmt.Println("Ready")
-		c.L.Unlock()
-		ch <- struct{}{}
-	}(c, ch)
+func listen(name string, data map[string]string, c *sync.Cond, wg *sync.WaitGroup) {
+	defer wg.Done()
+	c.L.Lock()
+	c.Wait()
 
+	fmt.Printf("[%s] %s\n", name, data["key"])
+
+	c.L.Unlock()
+
+	fmt.Printf("[%s] %s\n", name, data["key"])
+
+}
+
+func broadcast(name string, data map[string]string, c *sync.Cond) {
 	time.Sleep(time.Second)
 
 	c.L.Lock()
-	fmt.Println("Send")
-	c.L.Unlock()
-	c.Broadcast()
-	fmt.Println("Send unlock")
 
-	<-ch
+	data["key"] = "value"
+
+	fmt.Printf("[%s] данные получены\n", name)
+
+	c.Broadcast()
+	c.L.Unlock()
+}
+
+func main() {
+	data := map[string]string{}
+
+	cond := sync.NewCond(&sync.Mutex{})
+
+	wg := &sync.WaitGroup{}
+
+	wg.Add(2)
+
+	go listen("слушатель 1", data, cond, wg)
+	go listen("слушатель 2", data, cond, wg)
+
+	go broadcast("источник", data, cond)
+
+	wg.Wait()
+
 }
