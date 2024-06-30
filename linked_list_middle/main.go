41f72adfe2c0174a7ca0869bc5fe82fbec5c07f1
krivobokov <krivobokov_sv@magnit.ru>
2024-06-30 16:33:01 +0300

    middle linked list algorithm fix

--- linked_list_middle/main.go
+++ linked_list_middle/main.go
@@ -1,55 +1,29 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
+
+type Node struct {
+	val  int
+	next *Node
+}
 
 func main() {
-	data := [6]int{1, 2, 3, 4, 5, 6}
-
-	list := &linkedList{}
-	for _, val := range data {
-		el := &item{
-			value: val,
-		}
-		list.add(el)
-	}
-	fmt.Println(list.getMiddle().value)
+	n1 := &Node{val: 1}
+	n2 := &Node{val: 2}
+	n1.next = n2
+	n3 := &Node{val: 3}
+	n2.next = n3
+	fmt.Println(getMiddle(n1))
 }
 
-type item struct {
-	value int
-	next  *item
-}
-
-type linkedList struct {
-	root     *item
-	middle   *item
-	current  *item
-	curIndex int
-}
-
-func (list *linkedList) add(el *item) {
-	if list.root == nil {
-		list.root = el
-		list.middle = el
-		list.current = list.root
-	} else {
-		list.current.next = el
-		list.current = el
+func getMiddle(node *Node) *Node {
+	slow := node
+	fast := node
+	for fast != nil && fast.next != nil {
+		fast = fast.next.next
+		slow = slow.next
 	}
 
-	if list.curIndex%2 == 1 {
-		list.middle = list.middle.next
-	}
+	return slow
 
-	list.curIndex++
-}
-
-func (list *linkedList) getMiddle() *item {
-	return list.middle
-}
-
-func (list *linkedList) getCurrent() *item {
-	return list.current
 }
