a7732d0891447410cfdf7e4f955fc05e3fb0ae97
krivobokov <krivobokov_sv@magnit.ru>
2024-06-30 15:20:20 +0300

    arrays union algorithm fix

--- arrays_zip/main.go
+++ arrays_zip/main.go
@@ -3,24 +3,26 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println(zip([]int{1, 2}, []int{3, 4, 5}, []int{5, 6, 7, 8, 9}))
+	fmt.Println(union([]int{1, 2, 3, 7, 6, 4}, []int{6, 4, 3, 2, 90}, []int{7, 9, 3, 2, 1, 89, 90}, []int{3, 4, 5, 90}))
 }
 
-func zip(a ...[]int) [][]int {
-	l := len(a)
-	ml := len(a[0])
-	for i := 1; i < l; i++ {
-		ml = min(ml, len(a[i]))
-	}
-
-	res := make([][]int, ml)
-
-	for i := 0; i < ml; i++ {
-		res[i] = make([]int, 0)
-		for j := 0; j < l; j++ {
-			res[i] = append(res[i], a[j][i])
+func union(a ...[]int) [][]int {
+	l := len(a[0])
+	al := len(a)
+	for _, v := range a {
+		if len(v) < l {
+			l = len(v)
 		}
 	}
 
-	return res
+	u := make([][]int, 0, l)
+	for i := 0; i < l; i++ {
+		el := make([]int, 0, al)
+		for j := 0; j < al; j++ {
+			el = append(el, a[j][i])
+		}
+		u = append(u, el)
+	}
+
+	return u
 }
