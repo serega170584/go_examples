4b6cdf476adaf1f1deb6777c0e365ad90d05e40a
krivobokov <krivobokov_sv@magnit.ru>
2024-07-01 10:50:15 +0300

    worker pool algorithm fix

--- worker_pool/main.go
+++ worker_pool/main.go
@@ -2,29 +2,27 @@ package main
 
 import (
 	"fmt"
-	"strconv"
 	"sync"
 )
 
 func main() {
 	in := make(chan int, 1000)
-	out := make(chan string)
-
 	for i := 0; i < 1000; i++ {
 		in <- i
 	}
 	close(in)
 
-	wg := &sync.WaitGroup{}
-	wg.Add(4)
+	out := make(chan int)
 
-	for i := 0; i < 4; i++ {
-		go func(i int) {
+	wg := &sync.WaitGroup{}
+	wg.Add(5)
+	for i := 0; i < 5; i++ {
+		go func() {
 			defer wg.Done()
 			for v := range in {
-				out <- "Task: " + strconv.Itoa(i) + ", value: " + strconv.Itoa(v)
+				out <- v
 			}
-		}(i)
+		}()
 	}
 
 	go func() {
