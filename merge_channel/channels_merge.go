0417105f6ff4df735a75cfd391ff3c1876496ba9
krivobokov <krivobokov_sv@magnit.ru>
2024-07-03 13:12:52 +0300

    channels merge algorithm fix

--- merge_channel/channels_merge.go
+++ merge_channel/channels_merge.go
@@ -6,33 +6,31 @@ import (
 )
 
 func main() {
-	a := make(chan int, 3)
-	a <- 1
-	a <- 2
-	a <- 3
-	close(a)
-	b := make(chan int, 2)
-	b <- 4
-	b <- 5
-	close(b)
-	c := make(chan int, 4)
-	c <- 6
-	c <- 7
-	c <- 8
-	c <- 9
-	close(c)
-
-	out := merge(a, b, c)
-
+	ch1 := make(chan int, 4)
+	ch1 <- 1
+	ch1 <- 2
+	ch1 <- 3
+	ch1 <- 4
+	close(ch1)
+	ch2 := make(chan int, 3)
+	ch2 <- 5
+	ch2 <- 6
+	ch2 <- 7
+	close(ch2)
+	ch3 := make(chan int, 2)
+	ch3 <- 8
+	ch3 <- 9
+	close(ch3)
+	out := mergeChannels(ch1, ch2, ch3)
 	for v := range out {
 		fmt.Println(v)
 	}
 }
 
-func merge(chList ...chan int) chan int {
+func mergeChannels(chList ...chan int) chan int {
 	out := make(chan int)
-	wg := &sync.WaitGroup{}
 	l := len(chList)
+	wg := sync.WaitGroup{}
 	wg.Add(l)
 	for _, ch := range chList {
 		go func(ch chan int) {
