f45d3b57130473362a711e0eaa48c24b8b5cff09
krivobokov <krivobokov_sv@magnit.ru>
2025-01-05 13:13:32 +0300

    space count algorithm fix

--- space_count/main.go
+++ space_count/main.go
@@ -9,30 +9,26 @@ import (
 
 func main() {
 	scanner := bufio.NewScanner(os.Stdin)
-	scanner.Split(bufio.ScanWords)
-
 	scanner.Scan()
 	n, _ := strconv.Atoi(scanner.Text())
 
-	list := make([]int, n)
+	var list []int
 	for i := 0; i < n; i++ {
 		scanner.Scan()
-		list[i], _ = strconv.Atoi(scanner.Text())
+		v, _ := strconv.Atoi(scanner.Text())
+		list = append(list, v)
 	}
 
-	fmt.Println(getMinSyms(n, list))
-}
+	m := make(map[int]int, 4)
+	m[0] = 0
+	m[1] = 1
+	m[2] = 2
+	m[3] = 2
 
-func getMinSyms(n int, list []int) int {
-	syms := make([]int, 4)
-	syms[1] = 1
-	syms[2] = 2
-	syms[3] = 2
-
-	q := 0
+	cnt := 0
 	for _, v := range list {
-		q += v/4 + syms[v%4]
+		cnt += v/4 + m[v%4]
 	}
 
-	return q
+	fmt.Println(cnt)
 }
