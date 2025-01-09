4a1bbf9c8de2936d4a01750c760715c984f860dc
krivobokov <krivobokov_sv@magnit.ru>
2025-01-09 10:34:12 +0300

    startup profit algorithm fix

--- startup_profit/main.go
+++ startup_profit/main.go
@@ -5,26 +5,7 @@ import (
 	"fmt"
 	"os"
 	"strconv"
-	"strings"
 )
-
-//25 175 10
-//25 165 10
-//25 150 10
-//25 135 10
-//25 110 10
-//25 95 10
-//25 80 10
-//25 65 10
-//25 50 10
-//25 35 10
-//25 20 10
-//25 5 10
-//25 0 0
-
-// 2500 1500 2499
-// 2500 0 4998
-// 2500 0 2498
 
 func main() {
 	scanner := bufio.NewScanner(os.Stdin)
@@ -39,46 +20,33 @@ func main() {
 	scanner.Scan()
 	d, _ := strconv.Atoi(scanner.Text())
 
-	fmt.Println(getMaxStratupProfit(n, k, d))
-}
-
-func getMaxStratupProfit(n int, k int, d int) string {
-	m := 0
-
-	res := make([]int, 0)
-	delim := n
-	for delim != 0 {
-		res = append([]int{delim % 10}, res...)
-		delim /= 10
-	}
-
-	for i := 0; i < d; i++ {
-		for j := 0; j < 10; j++ {
-			m = (n + m) % k
+	add := strconv.Itoa(n)
+	if n == k {
+		add += "0"
+		d--
+	} else {
+		r := n
+		for i := 0; i < 9; i++ {
+			r += n
+			r = r % k
 		}
-
-		v := -1
-
-		for j := 0; j < 10; j++ {
-			n1 := m + j
-			if n1%k == 0 {
-				v = j
-				n = n1
+		if r == 0 {
+			add += "0"
+		} else {
+			addNum := k - r
+			if addNum > 9 {
+				fmt.Println("-1")
+				return
 			}
+			add += strconv.Itoa(addNum)
 		}
-
-		if v == -1 {
-			return "-1"
-		}
-
-		res = append(res, v)
+		d--
 	}
 
-	var sb strings.Builder
-
-	for _, v := range res {
-		sb.WriteString(strconv.Itoa(v))
+	for d > 0 {
+		add += "0"
+		d--
 	}
 
-	return sb.String()
+	fmt.Println(add)
 }
