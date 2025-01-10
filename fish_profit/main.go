36f81fd7d78c50a640bdae0cb99c4ad458e9a5d8
krivobokov <krivobokov_sv@magnit.ru>
2025-01-10 07:35:08 +0300

    fish profit algorithm fix

--- fish_profit/main.go
+++ fish_profit/main.go
@@ -17,26 +17,21 @@ func main() {
 	scanner.Scan()
 	k, _ := strconv.Atoi(scanner.Text())
 
-	prices := make([]int, n)
-
+	list := make([]int, n)
 	for i := 0; i < n; i++ {
 		scanner.Scan()
-		prices[i], _ = strconv.Atoi(scanner.Text())
+		list[i], _ = strconv.Atoi(scanner.Text())
 	}
 
-	fmt.Println(fishProfit(prices, n, k))
-}
-
-func fishProfit(prices []int, n int, k int) int {
 	maxProfit := 0
-	for i := 0; i < n-1; i++ {
-		last := i + k + 1
-		if i+k+1 > n {
-			last = n
+	for i := range list {
+		if i+k == n {
+			break
 		}
-		for j := i + 1; j < last; j++ {
-			maxProfit = max(maxProfit, prices[j]-prices[i])
+		for j := i + 1; j <= i+k; j++ {
+			maxProfit = max(maxProfit, list[j]-list[i])
 		}
 	}
-	return maxProfit
+
+	fmt.Println(maxProfit)
 }
