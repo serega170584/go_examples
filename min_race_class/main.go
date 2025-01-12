1ac47cd0c343fbf31baad8f6b710546e874c290a
krivobokov <krivobokov_sv@magnit.ru>
2025-01-12 10:35:08 +0300

    best deny algorithm fix

--- min_race_class/main.go
+++ min_race_class/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"math"
 	"os"
 	"strconv"
 )
@@ -12,78 +13,104 @@ func main() {
 	scanner.Split(bufio.ScanWords)
 
 	scanner.Scan()
-	m, _ := strconv.Atoi(scanner.Text())
-
-	scanner.Scan()
 	n, _ := strconv.Atoi(scanner.Text())
 
-	list := make([][]int, m)
-	for i := 0; i < m; i++ {
-		for j := 0; j < n; j++ {
+	scanner.Scan()
+	m, _ := strconv.Atoi(scanner.Text())
+
+	cost := make([][]int, n)
+	for i := 0; i < n; i++ {
+		for j := 0; j < m; j++ {
 			scanner.Scan()
 			v, _ := strconv.Atoi(scanner.Text())
-			list[i] = append(list[i], v)
+			cost[i] = append(cost[i], v)
 		}
 	}
 
-	fmt.Println(getMaxDenyPoint(list, m, n))
-}
-
-func getMaxDenyPoint(list [][]int, m int, n int) (int, int) {
-	rowMax1 := make([]int, m)
-	rowMax2 := make([]int, m)
-
-	for i, row := range list {
-		for _, v := range row {
-			if v > rowMax1[i] {
-				rowMax2[i] = rowMax1[i]
-				rowMax1[i] = v
-			} else if v > rowMax2[i] {
-				rowMax2[i] = v
+	rowDirectMax := make([]int, n)
+	rowInDirectMax := make([]int, n)
+	for i := 0; i < n; i++ {
+		rowDirectMax[i] = cost[i][0]
+		rowInDirectMax[i] = cost[i][1]
+		if rowInDirectMax[i] > rowDirectMax[i] {
+			rowDirectMax[i], rowInDirectMax[i] = rowInDirectMax[i], rowDirectMax[i]
+		}
+		for j := 2; j < m; j++ {
+			if cost[i][j] > rowDirectMax[i] {
+				rowDirectMax[i], rowInDirectMax[i] = cost[i][j], rowDirectMax[i]
+				continue
+			}
+			if cost[i][j] > rowInDirectMax[i] {
+				rowInDirectMax[i] = cost[i][j]
 			}
 		}
 	}
 
-	withExcludedMax := make([][]int, m)
-	for i, row := range list {
-		withExcludedMax[i] = make([]int, n)
-		for j, v := range row {
-			withExcludedMax[i][j] = rowMax1[i]
-			if v == rowMax1[i] {
-				withExcludedMax[i][j] = rowMax2[i]
-			}
-		}
-	}
-
-	excludedMax := make([][]int, m)
-	excludedMax[0] = make([]int, n)
-
-	for i := 1; i < m; i++ {
-		excludedMax[i] = make([]int, n)
+	colRestMax := make([][]int, m)
+	for i := 0; i < m; i++ {
+		colRestMax[i] = make([]int, n)
 		for j := 0; j < n; j++ {
-			excludedMax[i][j] = max(excludedMax[i-1][j], withExcludedMax[i-1][j])
-		}
-	}
-
-	for i := m - 2; i >= 0; i-- {
-		for j := n - 1; j >= 0; j-- {
-			excludedMax[i][j] = max(excludedMax[i+1][j], withExcludedMax[i+1][j])
-		}
-	}
-
-	minMaxVal := excludedMax[0][0]
-	rowIndex := 1
-	colIndex := 1
-	for i, row := range excludedMax {
-		for j, v := range row {
-			if v < minMaxVal {
-				minMaxVal = v
-				rowIndex = i + 1
-				colIndex = j + 1
+			colRestMax[i][j] = rowDirectMax[j]
+			if cost[j][i] == colRestMax[i][j] {
+				colRestMax[i][j] = rowInDirectMax[j]
 			}
 		}
 	}
 
-	return rowIndex, colIndex
+	downRowForColRestMax := make([][]int, n)
+	downRowForColRestMax[0] = make([]int, m)
+	downRowForColRestMax[1] = make([]int, m)
+	for j := 0; j < m; j++ {
+		downRowForColRestMax[1][j] = colRestMax[j][0]
+	}
+	for i := 2; i < n; i++ {
+		downRowForColRestMax[i] = make([]int, m)
+		for j := 0; j < m; j++ {
+			downRowForColRestMax[i][j] = downRowForColRestMax[i-1][j]
+			if colRestMax[j][i-1] > downRowForColRestMax[i][j] {
+				downRowForColRestMax[i][j] = colRestMax[j][i-1]
+			}
+		}
+	}
 
+	upRowForColRestMax := make([][]int, n)
+	for i := 0; i < n; i++ {
+		upRowForColRestMax[i] = make([]int, m)
+	}
+	for j := 0; j < m; j++ {
+		upRowForColRestMax[n-2][j] = colRestMax[j][n-1]
+	}
+	for i := n - 3; i >= 0; i-- {
+		for j := 0; j < m; j++ {
+			upRowForColRestMax[i][j] = upRowForColRestMax[i+1][j]
+			if colRestMax[j][i+1] > upRowForColRestMax[i][j] {
+				upRowForColRestMax[i][j] = colRestMax[j][i+1]
+			}
+		}
+	}
+
+	resRow := 0
+	resCol := 0
+	for i := 0; i < n; i++ {
+		for j := 0; j < m; j++ {
+			if downRowForColRestMax[i][j] < upRowForColRestMax[i][j] {
+				downRowForColRestMax[i][j] = upRowForColRestMax[i][j]
+				resRow = i
+				resCol = j
+			}
+		}
+	}
+
+	minMax := math.MaxInt
+	for i := 0; i < n; i++ {
+		for j := 0; j < m; j++ {
+			if downRowForColRestMax[i][j] < minMax {
+				minMax = downRowForColRestMax[i][j]
+				resRow = i
+				resCol = j
+			}
+		}
+	}
+
+	fmt.Println(resRow+1, resCol+1)
 }
