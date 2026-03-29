e251f92bcb55e11f21d2762d2f05f51f05c23d87
krivobokov <krivobokov_sv@magnit.ru>
2026-03-29 20:31:45 +0300

    adapter fix

--- adapter/main.go
+++ adapter/main.go
@@ -3,42 +3,41 @@ package main
 import (
 	"context"
 	"fmt"
-	"math"
 	"math/rand"
 	"time"
 )
 
 func main() {
-	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	ctx, cancel := context.WithTimeout(context.Background(), time.Duration(rand.Intn(5))*time.Second)
 	defer cancel()
 
-	res, err := adapter(ctx)
+	v, err := adapter(ctx)
+
 	if err != nil {
-		fmt.Println(err.Error())
+		fmt.Println("Error ", err)
 		return
 	}
 
-	math.
-		fmt.Println(*res)
+	fmt.Printf("Success %d", *v)
+}
+
+func something() int {
+	time.Sleep(3 * time.Second)
+	res := rand.Intn(10000)
+	return res
 }
 
 func adapter(ctx context.Context) (*int, error) {
 	ch := make(chan int)
+
 	go func() {
 		ch <- something()
 	}()
 
 	select {
-	case <-ctx.Done():
-		return nil, ctx.Err()
 	case v := <-ch:
 		return &v, nil
-	}
-}
-
-func something() int {
-	select {
-	case <-time.After(time.Duration(rand.Intn(5)) * time.Second):
-		return 123
+	case <-ctx.Done():
+		return nil, ctx.Err()
 	}
 }
