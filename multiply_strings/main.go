bb780f4ce96e76edecc290bdb8e453cfb6be9073
krivobokov <krivobokov_sv@magnit.ru>
2026-05-11 16:11:22 +0300

    multiply strings fix

--- multiply_strings/main.go
+++ multiply_strings/main.go
@@ -2,27 +2,17 @@ package main
 
 import "fmt"
 
-//		 99
-//		 99
-//		 81
-//	 81
-//	 891
-//	 81
-//
-// 1701
-// 99 * 99 = (90 + 9) *(90 + 9) = 90 * 90 + 90 * 9 + 9 * 90 + 9 * 9
 func main() {
 	a := []int{9, 9}
 	b := []int{9, 9}
 	c := make([]int, len(a)+len(b))
 	for i := len(a) - 1; i >= 0; i-- {
 		for j := len(b) - 1; j >= 0; j-- {
-			res := c[i+j+1] + a[i]*b[j]
-			lastRes := res % 10
-			firstRes := res / 10
-			c[i+j+1] = lastRes
-			c[i+j] += firstRes
+			v := a[i]*b[j] + c[i+j+1]
+			c[i+j+1] = v % 10
+			c[i+j] += v / 10
 		}
 	}
+
 	fmt.Println(c)
 }
