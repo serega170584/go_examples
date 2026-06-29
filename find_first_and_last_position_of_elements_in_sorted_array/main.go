c78aa508a8f51dcdadc682dd42851ff8eec361a2
krivobokov <krivobokov_sv@magnit.ru>
2026-06-29 19:23:12 +0300

    find first and last position in sorted fix

--- find_first_and_last_position_of_elements_in_sorted_array/main.go
+++ find_first_and_last_position_of_elements_in_sorted_array/main.go
@@ -1,7 +1,9 @@
 package main
 
+import "fmt"
+
 func main() {
-	nums := []int{5, 7, 7, 8, 8, 10}
+	nums := []int{5, 7, 7, 8, 8, 8, 8, 10}
 	target := 8
 
 	l := 0
@@ -10,12 +12,23 @@ func main() {
 		m := (l + r) / 2
 		if nums[m] < target {
 			l = m + 1
-			continue
+		} else {
+			r = m
 		}
-
-		r = m
 	}
+	first := l
 
-	ls := l
+	l = 0
+	r = len(nums) - 1
+	for l < r {
+		m := (l + r + 1) / 2
+		if nums[m] <= target {
+			l = m
+		} else {
+			r = m - 1
+		}
+	}
+	second := l
 
+	fmt.Println(first, second)
 }
