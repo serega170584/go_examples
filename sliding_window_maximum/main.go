b7d739c71ce2d75b5c6df6b487ce131fb983a4bb
krivobokov <krivobokov_sv@magnit.ru>
2026-07-12 18:14:22 +0300

    sliding window maximum fix

--- sliding_window_maximum/main.go
+++ sliding_window_maximum/main.go
@@ -5,37 +5,38 @@ import "fmt"
 func main() {
 	a := []int{1, 3, -1, -3, 5, 3, 6, 7}
 	k := 3
-	mi := 0
+	res := make([]int, 0)
+	indexes := make([]int, k)
+	indexes[0] = 0
 	for i := 1; i < k; i++ {
-		if a[i] > a[mi] {
-			mi = i
-		}
-	}
-
-	maxIndexes := make([]int, 0, len(a)-k)
-	maxIndexes = append(maxIndexes, mi)
-
-	res := make([]int, 0, len(a)-k)
-	res = append(res, a[mi])
-
-	for i := k; i < len(a); i++ {
-		if i-maxIndexes[0] == k {
-			maxIndexes = maxIndexes[1:]
-		}
-
-		for j := len(maxIndexes) - 1; j >= 0; j-- {
-			index := maxIndexes[j]
-			if a[i] < a[index] {
+		l := len(indexes)
+		for j := 0; j < l; j++ {
+			index := indexes[j]
+			if a[i] >= a[index] {
+				indexes = indexes[:j]
 				break
 			}
+		}
+		indexes = append(indexes, i)
+	}
 
-			maxIndexes = maxIndexes[:len(maxIndexes)-1]
+	res = append(res, a[indexes[0]])
+	for i := k; i < len(a); i++ {
+		index := indexes[0]
+		if i-index == k {
+			indexes = indexes[1:]
 		}
 
-		maxIndexes = append(maxIndexes, i)
-
-		index := maxIndexes[0]
-		res = append(res, a[index])
+		l := len(indexes)
+		for j := 0; j < l; j++ {
+			index = indexes[j]
+			if a[i] >= a[index] {
+				indexes = indexes[:j]
+				break
+			}
+		}
+		indexes = append(indexes, i)
+		res = append(res, a[indexes[0]])
 	}
 
 	fmt.Println(res)
